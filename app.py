35400bf084f41a2b7e5c853e602a82c394f41814
efgen42 <efgen42@gmail.com>
2020-06-24 14:17:32 +0700

    cgi version

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env  python3
+#!/usr/bin/env python3
 import datetime
 def do_magic():
     now = datetime.datetime.now()
     return "Hello! {0}".format(now)
 if __name__ == "__main__":
+    print("Content-type: text/html\n\n")
     print(do_magic())
